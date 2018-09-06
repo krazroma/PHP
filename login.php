9487216d7a6d7c3b69478fa38c71b73182d551da
krazroma <42806048+krazroma@users.noreply.github.com>
2018-09-06 10:51:33 -0400

    p

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 session_start();
 require('dbconnection.php');
 
-if (isset($_POST['username'])
+if (isset($_POST['username']))
 {
   $username = $_POST['username'];
   $password = $_POST['password'];
