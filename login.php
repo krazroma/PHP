41f5ea9091030ba5234cfcc290d76eacb949999f
krazroma <42806048+krazroma@users.noreply.github.com>
2018-09-06 10:49:34 -0400

    p

--- login.php
+++ login.php
@@ -50,7 +50,7 @@ if (isset($_POST['username'])
     </form>
 
     <?php
-      echo "Logged in as: " . $_SESSION['username'];    }
+      echo "Logged in as: " . $_SESSION['username'];
 
       //if (isset($username) && isset($password))
       //{
