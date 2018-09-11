4437f2cf30d019afecbf9396dcffac22365da233
krazroma <42806048+krazroma@users.noreply.github.com>
2018-09-11 10:28:31 -0400

    p

---
+++ upload.php
@@ -0,0 +1,15 @@
+<?php
+if (!isset($_SESSION))
+{
+  session_start();
+}
+
+if (!isser($_SESSION['username']))
+// die("Don`t even think about it");
+header('login.php');
+?>
+
+Upload your file.
+<form action="" method="post">
+  <input type="file" name="upload">
+</form>
