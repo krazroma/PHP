c5f3a686be25da842c695ad131b2ff0375f31b69
krazroma <42806048+krazroma@users.noreply.github.com>
2018-09-12 11:25:12 -0400

    p

--- upload.php
+++ upload.php
@@ -7,10 +7,14 @@ if (!isset($_SESSION))
 if (!isset($_SESSION['username']))
 {
   // die("Don`t even think about it");
-  header('login.php');
+  header('Location: login.php');
 }
 
-if (isset($_POST['upload']))
+var_dump($_POST['upload']);
+echo "<hr />";
+var_dump($_FILES['upload']);
+
+if ( isset($_FILES['upload']) )
 {
   $targer_dir = "uploads/";
   $targer_file = $targer_dir . basename($_FILES['upload']['name']);
