6da464d14100202c93ccfe929e0dd2b882adc6fc
krazroma <42806048+krazroma@users.noreply.github.com>
2018-09-14 10:17:57 -0400

    p

--- upload.php
+++ upload.php
@@ -18,7 +18,12 @@ if (isset($_FILES['upload']))
 {
   $target_dir = "uploads/";
   $target_file = $target_dir . basename($_FILES['upload']['name']);
-  move_uploaded_file($_FILES['upload']['tmp_name'], $target_file);
+  $uploadVerification = true;
+
+  if ($uploadVerification)
+  {
+    move_uploaded_file($_FILES['upload']['tmp_name'], $target_file);
+  }
 }
 ?>
 
