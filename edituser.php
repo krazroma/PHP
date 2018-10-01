b238c240f9e8c99cc3720d784f82efe96a407e9f
krazroma <42806048+krazroma@users.noreply.github.com>
2018-10-01 08:52:17 -0400

    p

--- edituser.php
+++ edituser.php
@@ -30,7 +30,7 @@ if(isset($_POST['username']) && isset($_POST['password']))
     $msg = "Error Updating";
     echo "$msg";
   }
-  var_dump($result);
+  //var_dump($result);
   header('Location: users.php');
 }
 
--- login.php
+++ login.php
@@ -1,6 +1,26 @@
 <?php
 session_start();
 require('dbconnection.php');
+require('nav.php');
+
+
+
+<?php
+if (!isset($_SESSION))
+{
+  session_start();
+}//checks if global variable for session is set
+if (!isset($_SESSION['username']))//check to see session is started
+{
+  header('Location: login.php');
+}
+ ?>
+  <br />
+ <a href="register.php">| Register</a>
+ <a href="upload.php">| Upload</a>
+ <a href="users.php">| Users</a>
+ <a href="login.php">| Log In</a>
+ <br />
 
 if (isset($_POST['username']))
 {
