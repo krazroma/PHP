78f83781ca28337de27682340cfdd8522a51e0ab
krazroma <42806048+krazroma@users.noreply.github.com>
2018-10-02 10:34:40 -0400

    p

--- register.php
+++ register.php
@@ -2,7 +2,11 @@
 if ($_SERVER['REQUEST_METHOD'] == 'POST')
 {
   require('dbconnection.php');
+  // grab POST data could be dangerous because of XSS (cross site inctiption) or SQL injection
   $username = $_POST['username'];
+  // sanitaze the $username by remove tags
+  $username = filter_var($username, FILTER_SANITAZE_STRING);
+  // grab POST data password will be hashed so no need to sanitaze
   $password = $_POST['password'];
   $password = password_hash($password, PASSWORD_BCRYT);
   $sql = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
