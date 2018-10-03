67ca70c4f04f0ad681d0c1f61f587ece179fb420
krazroma <42806048+krazroma@users.noreply.github.com>
2018-10-03 08:48:38 -0400

    p

--- login.php
+++ login.php
@@ -47,12 +47,12 @@ if (isset($_POST['username']))
 
     if(isset($_SESSION['username']))
     {
-      echo "<a href=\"upload.php\"><strong>| Upload</strong></a>";
+      echo "<a href=\"upload.php\">| Upload</a>";
     }
 
     if(isset($_SESSION['username']))
     {
-      echo "<a href=\"users.php\"><strong>| Users</strong></a>";
+      echo "<a href=\"users.php\">| Users</a>";
     }
   ?>
 
