bccc02cab5a209207996b5af30d71df061ec211e
krazroma <42806048+krazroma@users.noreply.github.com>
2018-10-08 21:11:35 -0400

    1

--- cookies.php
+++ cookies.php
@@ -5,27 +5,41 @@ $cookie_value = date("l jS \of F Y h:i:s A");// l -day of the week
 //setcookie($cookie_name,$cookie_value, time() + (86400*30), "/");
 //86400 = 1 day
 
-if (isset($_COOKIE['last_visit']))
-{
-  $notification = "You have been here within 30 days";
-  $last_visit = $_COOKIE['last_visit'];
+// if (isset($_COOKIE['last_visit']))
+// {
+//   $notification = "You have been here within 30 days";
+//   $last_visit = $_COOKIE['last_visit'];
+//
+//   //$cookie_value = time();
+//   //$last_visit = $_COOKIE['last_visit'];
+//   setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
+// }
+// else {
+//   $notification = "This is your first time here, or it`s been more than 30 days";
+//   setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
+// }
 
-  //$cookie_value = time();
-  //$last_visit = $_COOKIE['last_visit'];
-  setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
-}
-else {
-  $notification = "This is your first time here, or it`s been more than 30 days";
-  setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
-}
+// <?php
+//  $cookie_name ="user";
 
-if (isset($_COOKIE['last_visit']))
-{
-  $notification = "Last time you were here " . (time() - $_COOKIE['last_visit']) . " seconds ago";
-  $cookie_value = time();
-  // $change = time() - $cookie_value;
-  // $visit_time = "Last time you were here " . $change . " seconds ago";
-}
+ if (isset($_COOKIE[$cookie_name])){
+   echo "You were here last " .(time() - $_COOKIE[$cookie_name]). " seconds ago.";
+   $cookie_value = time();
+   setcookie($cookie_name, $cookie_value, time() + (86400*30), "/");
+ }
+ else {
+   echo "This is your first time here. We are required to let you know that we use cookies.";
+   $cookie_value = time();
+   setcookie($cookie_name, $cookie_value, time() + (86400*30), "/");
+ }
+
+// if (isset($_COOKIE['last_visit']))
+// {
+//   $notification = "Last time you were here " . (time() - $_COOKIE['last_visit']) . " seconds ago";
+//   $cookie_value = time();
+//   // $change = time() - $cookie_value;
+//   // $visit_time = "Last time you were here " . $change . " seconds ago";
+// }
 
  ?>
 
