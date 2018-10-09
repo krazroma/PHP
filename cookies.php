0c08acc9145d397166b2c5bba2f221e0993349e0
krazroma <42806048+krazroma@users.noreply.github.com>
2018-10-08 21:16:32 -0400

    1

--- cookies.php
+++ cookies.php
@@ -7,7 +7,7 @@ $cookie_value = date("l jS \of F Y h:i:s A");// l -day of the week
 
 if (isset($_COOKIE['last_visit']))
 {
-  echo "You were here last " .(time() - $_COOKIE['last_visit']). " seconds ago.";
+  $notification = "You were here last " .(time() - $_COOKIE['last_visit']). " seconds ago.";
   $cookie_value = time();
   setcookie($cookie_name, $cookie_value, time() + (86400*30), "/");
   // $notification = "You have been here within 30 days";
