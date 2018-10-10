058aca2abfdf320c504d6eee455ac9b3167e711b
krazroma <42806048+krazroma@users.noreply.github.com>
2018-10-10 11:21:55 -0400

    1

--- shell_exec.php
+++ shell_exec.php
@@ -1,7 +1,22 @@
 <?php
+
+
 $output = shell_exec('ls -lah');
 echo "<pre>$output</pre>";
 
 $pwd = shell_exec('$pwd');
 echo "<pre>$pwd</pre>";
+
+
+$filename = '/test';
+
+if (file_exists($filename))
+{
+    echo "The file $filename exists";
+}
+else
+{
+    echo "The file $filename does not exist";
+}
+
  ?>
