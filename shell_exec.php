e01605fa55e98e23b6df9a37755bc3b478779def
krazroma <42806048+krazroma@users.noreply.github.com>
2018-10-10 11:36:06 -0400

    1

--- shell_exec.php
+++ shell_exec.php
@@ -8,27 +8,30 @@ $pwd = shell_exec('$pwd');
 echo "<pre>$pwd</pre>";
 
 
-//$filename = '/test';
+//$folder_test = file_exists("test");
 
-// if (file_exists($filename))
-// {
-//     echo "The file $filename exists";
-// }
-// else
-// {
-//     echo "The file $filename does not exist";
-// }
 
-$dirname = $_POST["test"];
-$filename = "/folder/" . $dirname . "/";
+$filename = 'test';
 
-if (!file_exists($filename)) {
-    //mkdir("folder/" . $dirname, 0777);
-    //echo "The directory $dirname was successfully created.";
-    echo "The directory $dirname exists.";
-    exit;
-} else {
-    echo "The directory $dirname doesn`t exist.";
+if (file_exists($filename))
+{
+    echo "The file $filename exists";
 }
+else
+{
+    echo "The file $filename does not exist";
+}
+
+// $dirname = $_POST["test"];
+// $filename = "/folder/" . $dirname . "/";
+//
+// if (!file_exists($filename)) {
+//     //mkdir("folder/" . $dirname, 0777);
+//     //echo "The directory $dirname was successfully created.";
+//     echo "The directory $dirname exists.";
+//     exit;
+// } else {
+//     echo "The directory $dirname doesn`t exist.";
+// }
 
  ?>
