a91015b9f6faa975c5bc5076b80b296584f38aed
krazroma <42806048+krazroma@users.noreply.github.com>
2018-10-15 06:00:41 -0400

    1

--- shell_exec.php
+++ shell_exec.php
@@ -42,7 +42,7 @@ $file_test = file_exists("test");
     $users = shell_exec('w');
     echo "<pre>$users</pre>";
 
-    var_dump($users);
+    //var_dump($users);
 
     $exploded = multiexplode(array(",",".","|",":"),$users);
     print_r($exploded[12]);
