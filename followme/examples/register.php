cadc8e785b2b132944e67ffed381bd9c7a62b86e
krazroma <42806048+krazroma@users.noreply.github.com>
2018-10-16 10:22:42 -0400

    1

--- followme/examples/register.php
+++ followme/examples/register.php
@@ -1,4 +1,31 @@
-<!doctype html>
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+  require('dbconnection.php');
+  // grab POST data could be dangerous because of XSS (cross site inctiption) or SQL injection
+  $username = $_POST['username'];
+  // sanitaze the $username by remove tags
+  $username = filter_var($username, FILTER_SANITAZE_STRING);
+  // trim white space from beginning and the end of the $username
+  $username = trim($username);
+  // remove slashes from $username, no / allowed
+  //$username = stripslashes($username);
+
+  $username = str_replace("/", "", $username);
+  $username = str_replace("\\", "", $username);
+  // remove white space from the middle of the string
+  //$username = str_replace(' ', '', $username); //first parameter is string to look, second is what to replace with
+  $username = preg_replace("/\s+/", "", $username); //removes tab is pressed
+
+  // grab POST data password will be hashed so no need to sanitaze
+  $password = $_POST['password'];
+  $password = password_hash($password, PASSWORD_BCRYT);
+  $sql = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
+  $conn->query($sql);
+}
+ ?>
+
+ <!doctype html>
 <html lang="en">
 <head>
 	<meta charset="utf-8" />
