ce789d8525881bffb53423eb08d5cecc655a2c9e
krazroma <42806048+krazroma@users.noreply.github.com>
2018-10-16 10:42:55 -0400

    1

---
+++ followme/examples/dbconnection.php
@@ -0,0 +1,12 @@
+<?php
+// setting up the Database connection
+$db_host = 'localhost'; // Database is installed on the PHPH server
+$db_user = 'roman'; // name to log in to MySQL
+$db_password = 'southhills#'; // password to login to MySQL
+$db_name = 'roman'; // name of the database within MySQL
+$conn = new mysqli($db_host, $db_user, $db_password, $db_name);
+if ($conn->connect_error)
+{
+  die("Connection Failed: " . $conn->connect_error);
+}
+?>
