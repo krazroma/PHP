8c89f17a3656509b9e7efd4f3c3cb72c54caf930
krazroma <42806048+krazroma@users.noreply.github.com>
2018-10-16 23:55:57 -0400

    1

--- followme/examples/register.php
+++ followme/examples/register.php
@@ -12,21 +12,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
   {
     die("Connection Failed: " . $conn->connect_error);
   }
-  // grab POST data could be dangerous because of XSS (cross site inctiption) or SQL injection
-  $user_email = $_POST['user_email'];
-  // sanitaze the $username by remove tags
-  $user_email = filter_var($user_email, FILTER_SANITAZE_STRING);
-  // trim white space from beginning and the end of the $username
-  $user_email = trim($user_email);
-  // remove slashes from $username, no / allowed
-  //$username = stripslashes($username);
-
-  // $user_email = str_replace("/", "", $user_email);
-  // $user_email = str_replace("\\", "", $user_email);
-  // // remove white space from the middle of the string
-  // //$username = str_replace(' ', '', $username); //first parameter is string to look, second is what to replace with
-  $user_email = preg_replace("/\s+/", "", $user_email); //removes tab is pressed
-
   // grab POST data password will be hashed so no need to sanitaze
   $user_password = $_POST['user_password'];
   $user_password_hashed = password_hash($user_password, PASSWORD_BCRYPT);
