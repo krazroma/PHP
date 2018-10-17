9bf70c8bc496d6b4506e118714e0722e7bff7233
krazroma <42806048+krazroma@users.noreply.github.com>
2018-10-16 23:01:26 -0400

    1

--- followme/examples/register.php
+++ followme/examples/register.php
@@ -148,7 +148,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
                                     else
                                     {
                                       $msg = "Updated Sussecfully";
-                                      echo "$msg";.
+                                      echo "$msg";
                                     }
 
                                     ?>
