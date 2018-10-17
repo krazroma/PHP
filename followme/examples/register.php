e6459feb91a39b4687c13714a73a51ec8761db3f
krazroma <42806048+krazroma@users.noreply.github.com>
2018-10-16 23:43:51 -0400

    1

--- followme/examples/register.php
+++ followme/examples/register.php
@@ -30,6 +30,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
   // grab POST data password will be hashed so no need to sanitaze
   $user_password = $_POST['user_password'];
   $user_password_hashed = password_hash($user_password, PASSWORD_BCRYT);
+  var_dump($user_password_hashed);
   $sql = "INSERT INTO fm_users (user_email, user_password) VALUES ('$user_email','$user_password_hashed')";
   $conn->query($sql);
 }
@@ -134,19 +135,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
                                     <label>Password</label>
                                     <input type="password" name="user_password" class="form-control" placeholder="Password">
                                     <input type="submit" value="Register" button class="btn btn-danger btn-block btn-round">
-
-                                    <?php
-
-                                    if (mysqli_query($conn, $sql))
-                                    {
-                                        echo "<script>alert('Updated Sucessfully!!!');</script>";
-                                    }
-                                    else
-                                    {
-                                        die(mysqli_error($con));
-                                    }
-
-                                    ?>
                                 </form>
                                 <div class="forgot">
                                     <a href="#" class="btn btn-link btn-danger">Forgot password?</a>
