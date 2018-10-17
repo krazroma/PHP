9f41fa11ec08735fbafe48884bb2a9cc093e75a5
krazroma <42806048+krazroma@users.noreply.github.com>
2018-10-16 23:18:47 -0400

    1

--- followme/examples/register.php
+++ followme/examples/register.php
@@ -30,7 +30,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
   // grab POST data password will be hashed so no need to sanitaze
   $user_password = $_POST['user_password'];
   $user_password = password_hash($user_password, PASSWORD_BCRYT);
-  $sql = "INSERT INTO fm_users (user_email, user_password) VALUES ('$user_email', '$user_password')";
+  $sql = "INSERT INTO fm_users (user_email,user_password) VALUES ('$user_email','$user_password')";
   $conn->query($sql);
 
 
