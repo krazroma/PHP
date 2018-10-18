99d062ae3cce06a11a9a2eaa369679c8485305d5
krazroma <42806048+krazroma@users.noreply.github.com>
2018-10-18 06:09:13 -0400

    1

--- followme/examples/login.php
+++ followme/examples/login.php
@@ -1,10 +1,19 @@
 <?php
 
-session_start();
+//session_start();
 
  if ($_SERVER['REQUEST_METHOD'] == 'POST')
  {
-   require('dbconnection.php');
+   $db_host = 'localhost'; // Database is installed on the PHPH server
+   $db_user = 'roman'; // name to log in to MySQL
+   $db_password = 'southhills#'; // password to login to MySQL
+   $db_name = 'roman'; // name of the database within MySQL
+   $conn = new mysqli($db_host, $db_user, $db_password, $db_name);
+   if ($conn->connect_error)
+   {
+     die("Connection Failed: " . $conn->connect_error);
+   }
+   
    $user_email = $_POST['user_email'];
    $user_email = filter_var($user_email, FILTER_SANITIZE_EMAIL);
    if (filter_var($user_email, FILTER_VALIDATE_EMAIL))
