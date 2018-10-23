a47d9e954c3b5e4d6decdb2a561d3a9c900e83a8
krazroma <42806048+krazroma@users.noreply.github.com>
2018-10-23 08:38:34 -0400

    1

--- followme/examples/profile.php
+++ followme/examples/profile.php
@@ -7,7 +7,7 @@ start_session();
 
 require('dbconnection.php');
 
-$sql = "SELECT * FROM fm_users where user_email = " . $_POST['user_email'];
+$sql = "SELECT * FROM fm_users where user_email = '$user_email'";
 
 // execute the sql and return array to $result
 $result = $conn->query($sql);
