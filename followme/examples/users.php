f593975ed836d04bb0b08257a69039d9fe0e7b97
krazroma <42806048+krazroma@users.noreply.github.com>
2018-10-26 11:18:36 -0400

    1

--- followme/examples/users.php
+++ followme/examples/users.php
@@ -94,7 +94,7 @@ if (isset($_POST['user_id']))
                   <img src="../assets/img/faces/clem-onojeghuo-2.jpg" alt="Circle Image" class="img-circle img-no-padding img-responsive">
                 </div>
                 <div class="col-md-7 col-sm-4  ml-auto mr-auto">
-                  <h6><?php echo $_SESSION['first_name'] && echo $_SESSION['last_name']; ?><br/><small><?php echo $_SESSION['title']; ?></small></h6>
+                  <h6><?php echo $_SESSION['first_name'] echo $_SESSION['last_name']; ?><br/><small><?php echo $_SESSION['title']; ?></small></h6>
                 </div>
                 <div class="col-md-3 col-sm-2  ml-auto mr-auto">
                   <div class="form-check">
