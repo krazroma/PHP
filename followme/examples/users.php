592e0bf9e662c179a77da82bf41cc63f61d8c2e5
krazroma <42806048+krazroma@users.noreply.github.com>
2018-10-26 11:59:51 -0400

    1

--- followme/examples/users.php
+++ followme/examples/users.php
@@ -11,13 +11,7 @@ if (isset($_POST['user_id']))
 
   //echo "<form action=\"\" method=\"post\">";
 
-  while($row = $result->fetch_assoc())
-  {
-    $_SESSION['first_name'] = $row['first_name'];
-    $_SESSION['last_name'] = $row['last_name'];
-    $_SESSION['image_url'] = $row['image_url'];
-    $_SESSION['title'] = $row['title'];
-  }
+
 }
 
 // get array of users from Database
@@ -88,13 +82,17 @@ if (isset($_POST['user_id']))
       <div class="row">
         <div class="col-md-6 ml-auto mr-auto">
           <ul class="list-unstyled follows">
+
+          <?php while($row = $result->fetch_assoc())
+            {
+
             <li>
               <div class="row">
                 <div class="col-md-2 col-sm-2 ml-auto mr-auto">
-                  <img src="<?php echo $_SESSION['image_url']; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
+                  <img src="<?php echo $row['image_url']; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
                 </div>
                 <div class="col-md-7 col-sm-4  ml-auto mr-auto">
-                  <h6><?php echo $_SESSION['first_name']; echo " "; echo $_SESSION['last_name'];?><br/><small><?php echo $_SESSION['title']; ?></small></h6>
+                  <h6><?php echo $row['first_name']; echo " "; echo $row['last_name'];?><br/><small><?php echo $row['title']; ?></small></h6>
                 </div>
                 <div class="col-md-3 col-sm-2  ml-auto mr-auto">
                   <div class="form-check">
@@ -106,6 +104,8 @@ if (isset($_POST['user_id']))
                 </div>
               </div>
             </li>
+          }
+             ?>
             <hr />
           </ul>
         </div>
