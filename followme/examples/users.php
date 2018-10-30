369f9d4288d13d41b3bad003a2a316a190538f13
krazroma <42806048+krazroma@users.noreply.github.com>
2018-10-29 21:56:13 -0400

    1

--- followme/examples/users.php
+++ followme/examples/users.php
@@ -95,7 +95,7 @@ $row2 = $result2->fetch_assoc();
                   <div class="form-check">
                     <label class="form-check-label">
                       <input class="form-check-input" type="checkbox" value="" <?php
-                      if ($row2==122)
+                      if ($row2=='122')
                       {
                         echo checked;
                       }
