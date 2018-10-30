279a72d2963c95eeeb47adf13936cb0c479278d9
krazroma <42806048+krazroma@users.noreply.github.com>
2018-10-30 01:08:00 -0400

    1

--- followme/examples/users.php
+++ followme/examples/users.php
@@ -104,7 +104,7 @@ $result2 = $conn->query($sql2);
 
                       while($row2 = $result2->fetch_assoc();)
                       {
-                        if($_SESSION['user_id'] == $row['user_id'])
+                        if($_SESSION['user_id'] == $row2['user_id'])
                         {
                           echo checked;
                         }
