08b6efb635ae6005a4748daa30524be1ae750146
krazroma <42806048+krazroma@users.noreply.github.com>
2018-10-30 10:49:11 -0400

    1

--- followme/examples/users.php
+++ followme/examples/users.php
@@ -14,13 +14,14 @@ $user_id = $_SESSION['user_id'];
 $sql2 = "SELECT following_user_id FROM fm_follows WHERE user_id = '$user_id'";
 $result2 = $conn->query($sql2);
 
-$row2 = $result2->fetch_row();
-// while($row2 = $result2->fetch_row())
-// {
-//   $folliwing_user_ids[] = $row2[0];
-// }
+//$row2 = $result2->fetch_row();
+while($row2 = $result2->fetch_row())
+{
+  $folliwing_user_ids[] = $row2[0];
+  var_dump($folliwing_user_ids[]);
+}
 
-var_dump($row2);
+
 
 // get array of users from Database
 // <li> needs to be in the loop of database records
