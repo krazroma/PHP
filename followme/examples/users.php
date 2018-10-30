356612c5a07b2e794c4afd49b96d0b527d0c02d0
krazroma <42806048+krazroma@users.noreply.github.com>
2018-10-30 00:33:55 -0400

    1

--- followme/examples/users.php
+++ followme/examples/users.php
@@ -110,7 +110,7 @@ var_dump($row2);
                   <img src="<?php echo $row['image_url']; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
                 </div>
                 <div class="col-md-7 col-sm-4  ml-auto mr-auto">
-                  <h6><?php echo $row['first_name']; echo " "; echo $row['last_name'] echo " "; echo $row['user_id'];?><br/><small><?php echo $row['title']; ?></small></h6>
+                  <h6><?php echo $row['first_name']; echo " "; echo $row['last_name']; echo " "; echo $row['user_id'];?><br/><small><?php echo $row['title']; ?></small></h6>
                 </div>
                 <div class="col-md-3 col-sm-2  ml-auto mr-auto">
                   <div class="form-check">
