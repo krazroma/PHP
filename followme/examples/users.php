f50e431447757d023808603750d291a86d89a508
krazroma <42806048+krazroma@users.noreply.github.com>
2018-11-06 03:07:47 -0500

    1

--- followme/examples/users.php
+++ followme/examples/users.php
@@ -163,7 +163,7 @@ while($row2 = $result2->fetch_assoc())
                               }
                           ?>name="first_name" value="1">
 
-<input type="submit" value="Submit" button class="btn btn-danger btn-block btn-round">
+
                             <span class="form-check-sign"></span>
                         </label>
                       </div>
@@ -172,7 +172,7 @@ while($row2 = $result2->fetch_assoc())
                 </li>
                 <hr />
               <?php } ?>
-
+<input type="submit" value="Submit" button class="btn btn-danger btn-block btn-round">
             <!-- </form> -->
           </ul>
         </div>
