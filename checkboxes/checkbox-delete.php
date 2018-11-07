7f0e016d6b1611a033907906d58df5a6c0e62a50
krazroma <42806048+krazroma@users.noreply.github.com>
2018-11-06 19:53:57 -0500

    1

--- checkboxes/checkbox-delete.php
+++ checkboxes/checkbox-delete.php
@@ -1,97 +1,103 @@
-<html><head><title>Checkbox-edit</title>
-<link rel="stylesheet" href="css/checkbox.css" />
-</head>
-<div class="container2">
-<div class="main2">
-<br/>
-<br/>
-<br/>
-<?php
-$connection = mysql_connect('localhost','root','');
-$db=mysql_select_db('store',$connection);
-$query=mysql_query("select*from checkbox",$connection);
-echo"CLICK ON THE DATA YOU WANT TO DELETE";
-echo"<br/><br/><br>";
-echo "<ol>";
-while($row=mysql_fetch_array($query)){
-echo"<li><a href="checkbox-delete.php?uid={$row["user_id"]}">{$row["services"]}</a></li><br/>";
-}
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <title>Checkbox-edit</title>
+    <link rel="stylesheet" href="css/checkbox.css" />
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <div class="container2">
+      <div class="main2"><br/>
+        <?php
+          $connection = mysql_connect('localhost','root','');
+          $db = mysql_select_db('store',$connection);
+          $query = mysql_query("select*from checkbox",$connection);
+          echo"CLICK ON THE DATA YOU WANT TO DELETE";
+          echo"<br/><br/><br>";
+          echo "<ol>";
+          while($row=mysql_fetch_array($query))
+          {
+            echo"<li><a href="checkbox-delete.php $uid={$row["user_id"]}">{$row["services"]}</a></li><br/>";
+          }
 
-if(isset($_GET['uid'])){
-$id=$_GET['uid'];
-$query2=mysql_query("select * from checkbox where user_id=$id",$connection);
-while($row=mysql_fetch_array($query2))
-{
-$str=($row['services']);
-$data=(explode(",",$str));
+          if(isset($_GET['uid']))
+          {
+            $id=$_GET['uid'];
+            $query2=mysql_query("select * from checkbox where user_id=$id",$connection);
 
-$check=array("Website Development","Data Support","Online Marketing","Business Development","Responsive Themes");
-if (in_array($check[0], $data))
-{
-$checked1 ="checked";
-}
-else
-{
-$checked1 ="";
-}
+            while($row=mysql_fetch_array($query2))
+            {
+              $str=($row['services']);
+              $data=(explode(",",$str));
 
-if (in_array($check[1], $data))
-{
-$checked2 ="checked";
-}
-else
-{
-$checked2 ="";
-}
-if (in_array($check[2], $data))
-{
-$checked3 ="checked";
-}
-else
-{
-$checked3 ="";
-}
-if (in_array($check[3], $data))
-{
-$checked4 ="checked";
-}
-else
-{
-$checked4 ="";
-}
-if (in_array($check[4], $data))
-{
-$checked5 ="checked";
-}
-else
-{
-$checked5 ="";
-}
+              $check=array("Website Development","Data Support","Online Marketing","Business Development","Responsive Themes");
+              if (in_array($check[0], $data))
+              {
+              $checked1 ="checked";
+              }
+              else
+              {
+              $checked1 ="";
+              }
 
-echo "<form action='checkbox-delete.php?uid={$row ['user_id']}' method='POST'>
+              if (in_array($check[1], $data))
+              {
+              $checked2 ="checked";
+              }
+              else
+              {
+              $checked2 ="";
+              }
+              if (in_array($check[2], $data))
+              {
+              $checked3 ="checked";
+              }
+              else
+              {
+              $checked3 ="";
+              }
+              if (in_array($check[3], $data))
+              {
+              $checked4 ="checked";
+              }
+              else
+              {
+              $checked4 ="";
+              }
+              if (in_array($check[4], $data))
+              {
+              $checked5 ="checked";
+              }
+              else
+              {
+              $checked5 ="";
+              }
 
-<label class="heading">Select the Services you want to insert:</label><br/><br/>
-<input type='checkbox' name='check_list[]' value='Website Development' $checked1 ><label>Website Development</label><br/>
-<input type='checkbox' name='check_list[]' value='Data Support' $checked2 ><label>Data Support</label><br/>
-<input type='checkbox' name='check_list[]' value='Online Marketing'$checked3 ><label>Online Marketing</label><br/>
-<input type='checkbox' name='check_list[]' value='Business Development'$checked4 ><label>Business Development</label><br/>
-<input type='checkbox' name='check_list[]' value='Responsive Themes'$checked5><label>Responsive Themes</label><br/><br/>
+              echo "<form action='checkbox-delete.php $uid = {$row ['user_id']}' method='POST'>
 
-<input type='submit' name='dsubmit' Value='DELETE'/>";
-}
-}
-if(isset($_POST['dsubmit'])){
-$id=$_GET['uid'];
-$sql="DELETE FROM checkbox WHERE user_id=$id";
-$query2=mysql_query($sql,$connection);
-echo"<br/><br/><br/>";
-echo"Data Deleted Successfully...!!!";
-}
-echo"</ol>";
-mysql_close($connection);
-echo"<br/><br/>";
-echo"<a href="demo_checkbox.php">HOME</a>";
-?>
-</div>
-</div>
+              <label class="heading">Select the Services you want to insert:</label><br/><br/>
+              <input type='checkbox' name='check_list[]' value='Website Development' $checked1 ><label>Website Development</label><br/>
+              <input type='checkbox' name='check_list[]' value='Data Support' $checked2 ><label>Data Support</label><br/>
+              <input type='checkbox' name='check_list[]' value='Online Marketing'$checked3 ><label>Online Marketing</label><br/>
+              <input type='checkbox' name='check_list[]' value='Business Development'$checked4 ><label>Business Development</label><br/>
+              <input type='checkbox' name='check_list[]' value='Responsive Themes'$checked5><label>Responsive Themes</label><br/><br/>
+              <input type='submit' name='dsubmit' Value='DELETE'>";
+            }
+          }
+          if(isset($_POST['dsubmit']))
+          {
+            $id=$_GET['uid'];
+            $sql="DELETE FROM checkbox WHERE user_id=$id";
+            $query2=mysql_query($sql,$connection);
+            echo"<br/><br/><br/>";
+            echo"Data Deleted Successfully...!!!";
+          }
+          echo"</ol>";
+          mysql_close($connection);
+          echo"<br/><br/>";
+          echo"<a href="demo_checkbox.php">HOME</a>";
+        ?>
+      </div>
+    </div>
+  </body>
 </html>
--- checkboxes/demo_checkbox.php
+++ checkboxes/demo_checkbox.php
@@ -1,32 +1,33 @@
 <html>
-<head>
-<title>Check Box Demo</title>
-<link rel="stylesheet" href="css/checkbox.css" />
-</head>
-<body>
-<div class="container">
-<div class="main">
-<h2>Select the Services you want:</h2><hr/>
-<form action="checkbox.php" method="post">
-<label class="heading"></label><br/><br/>
-<input type="checkbox" name="check_list[]" value="Website Development"><label>Website Development</label><br/>
-<input type="checkbox" name="check_list[]" value="Data Support"><label>Data Support</label><br/>
-<input type="checkbox" name="check_list[]" value="Online Marketing"><label>Online Marketing</label><br/>
-<input type="checkbox" name="check_list[]" value="Business Development"><label>Business Development</label><br/>
-<input type="checkbox" name="check_list[]" value="Responsive Themes"><label>Responsive Themes</label><br/><br/>
-<input type="submit" name="submit" Value="Submit">
-<br/><br/>
-<h2>Update or Delete existing data.</h2><hr/><br/>
-<a class="link1" href=checkbox-edit.php>Edit</a><br/><br/><br/>
-<a class="link2" href=checkbox-delete.php>Delete</a>
-</form>
-</div>
-<br/>
-<!-- Div Fugo is advertisement div-->
-<div class="fugo">
-<a href="https://www.formget.com/app/"><img src="images/formGetadv-1.jpg" /></a>
-</div>
-</div>
-</body>
-
+  <head>
+  <title>Check Box Demo</title>
+  <link rel="stylesheet" href="css/checkbox.css">
+  </head>
+  <body>
+    <div class="container">
+    <div class="main">
+    <h2>Select the Services you want:</h2>
+    <hr/>
+    <form action="checkbox.php" method="post">
+      <label class="heading"></label><br/><br/>
+      <input type="checkbox" name="check_list[]" value="Website Development"><label>Website Development</label><br/>
+      <input type="checkbox" name="check_list[]" value="Data Support"><label>Data Support</label><br/>
+      <input type="checkbox" name="check_list[]" value="Online Marketing"><label>Online Marketing</label><br/>
+      <input type="checkbox" name="check_list[]" value="Business Development"><label>Business Development</label><br/>
+      <input type="checkbox" name="check_list[]" value="Responsive Themes"><label>Responsive Themes</label><br/><br/>
+      <input type="submit" name="submit" Value="Submit">
+      <br/>
+      <br/>
+      <h2>Update or Delete existing data.</h2><hr/><br/>
+      <a class="link1" href=checkbox-edit.php>Edit</a><br/><br/><br/>
+      <a class="link2" href=checkbox-delete.php>Delete</a>
+    </form>
+    </div>
+    <br/>
+    <!-- Div Fugo is advertisement div-->
+    <div class="fugo">
+      <a href="https://www.formget.com/app/"><img src="images/formGetadv-1.jpg" /></a>
+    </div>
+    </div>
+  </body>
 </html>
