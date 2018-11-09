9288466b31173195d5fc8264a7b062c3554af71c
krazroma <42806048+krazroma@users.noreply.github.com>
2018-11-09 10:27:34 -0500

    1

--- followme/examples/users.php
+++ followme/examples/users.php
@@ -53,7 +53,7 @@ while($row2 = $result2->fetch_assoc())
   $folliwing_user_ids[] = $row2['following_user_id'];
 }
 
-if (isset( $_POST["Submit"]))
+if (isset( $_POST["submit"]))
 {
   header( "Location: profile.php" );
   //exit;
