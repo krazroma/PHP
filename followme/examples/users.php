73eedff76e79d835a61864dcf75ec32e55afdf2b
krazroma <42806048+krazroma@users.noreply.github.com>
2018-11-13 10:18:54 -0500

    q

--- followme/examples/users.php
+++ followme/examples/users.php
@@ -7,7 +7,7 @@ require('dbconnection.php');
 if (isset($_POST["logout"]))
 {
   unset($_SESSION['user_email']);
-  header("location:login.php");
+  //header("location:login.php");
 }
 
 $MAIN_user_id = $_SESSION['user_id'];
@@ -118,7 +118,7 @@ if (isset( $_POST["submit"]))
               <a href="#" class="nav-link"><?php echo $_SESSION['user_email']; ?></a>
             </li>
             <li class="nav-item">
-              <a type="submit" name="logout" href="#" class="nav-link">Logout</a>
+              <a type="submit" name="logout" href="login.php" class="nav-link">Logout</a>
             </li>
           </ul>
         </form>
