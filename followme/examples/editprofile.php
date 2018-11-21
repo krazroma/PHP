c0444b96e1f4e26f2253424bf32accffbe47655d
krazroma <42806048+krazroma@users.noreply.github.com>
2018-11-20 21:07:27 -0500

    1

--- followme/examples/editprofile.php
+++ followme/examples/editprofile.php
@@ -2,20 +2,20 @@
 // strt session if it is not running
 
 session_start();
-
+require('dbconnection.php');
 
 // if (isset($_POST))
 if($_SERVER['REQUEST_METHOD'] == 'POST')
 {
-  $db_host = 'localhost'; // Database is installed on the PHPH server
-  $db_user = 'roman'; // name to log in to MySQL
-  $db_password = 'southhills#'; // password to login to MySQL
-  $db_name = 'roman'; // name of the database within MySQL
-  $conn = new mysqli($db_host, $db_user, $db_password, $db_name);
-  if ($conn->connect_error)
-  {
-    die("Connection Failed: " . $conn->connect_error);
-  }
+  // $db_host = 'localhost'; // Database is installed on the PHPH server
+  // $db_user = 'roman'; // name to log in to MySQL
+  // $db_password = 'southhills#'; // password to login to MySQL
+  // $db_name = 'roman'; // name of the database within MySQL
+  // $conn = new mysqli($db_host, $db_user, $db_password, $db_name);
+  // if ($conn->connect_error)
+  // {
+  //   die("Connection Failed: " . $conn->connect_error);
+  // }
 
 
   $sql ="UPDATE fm_users SET first_name='".$_POST['first_name']."', last_name='".$_POST['last_name']."',
@@ -33,7 +33,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST')
               $_SESSION['last_name'] = $row['last_name'];
               $_SESSION['title'] = $row['title'];
               $_SESSION['description'] = $row['description'];
-              header('Location: profile.php');
+              // header('Location: profile.php');
           }
     }
   }
@@ -46,6 +46,52 @@ if($_SERVER['REQUEST_METHOD'] == 'POST')
 
 // update session variables with new values
 
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+if($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+        $target_dir = "images/";
+        $target_file = $target_dir . basename($_FILES['upload']['name']);
+        $uploadVerification=true;
+        if (file_exists($target_file)) {  $uploadVerification=false;  $ret = "Sorry file already exists";}
+        $file_type = $_FILES['upload']['type'];
+
+        switch ($file_type)
+        {
+            case "image/jpeg":$uploadVerification = true; $img_type=".jpg";    break;
+            case "image/png":    $uploadVerification = true;    $img_type=".png"; break;
+            case "image/gif":    $uploadVerification = true;    $img_type=".gif"; break;
+            default: $uploadVerification = false;
+            $ret = "Sorry only jpg, png, and gif files are allowed!";
+        }
+
+        if (file_exists($target_file)) {  $uploadVerification=false;  $ret = "Sorry file already exists";}
+
+        if ($_FILES['upload']['size'] > 1000000){ $uploadVerification=false; $ret = "Sorry file is too big"; }
+
+        $target_file = $target_dir . $_SESSION['userid'] . $img_type; //NEW
+        if ($uploadVerification){move_uploaded_file($_FILES['upload']['tmp_name'], $target_file);
+
+            $sql2 ="UPDATE fm_users SET image='$target_file' WHERE userid = " . $_SESSION['userid'];
+            $result2 = $conn->query($sql2);
+        }
+
+        $sql_u ="UPDATE fm_users SET firstname='".$_POST['first_name']."', last_name='".$_POST['last_name']."',title='".$_POST['title']."', description='".$_POST['description']."' WHERE user_id = " . $_SESSION['user_id'];
+        $result_update = $conn->query($sql_u);
+
+        header('Location: profile.php');
+}// ends if post server access
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
 ?>
 <!doctype html>
 <html lang="en">
@@ -55,7 +101,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST')
 		<link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
-		<title>Profile page</title>
+		<title>Edit Profile page</title>
 
 		<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
 	  <meta name="viewport" content="width=device-width" />
@@ -108,15 +154,17 @@ if($_SERVER['REQUEST_METHOD'] == 'POST')
 					<div class="row">
 							<div class="col-md-8 ml-auto mr-auto">
 								<h2 class="text-center">Edit Profile</h2>
+
 								<form class="contact-form" action="" method="post">
 								<div class="row">
+
 									<div class="col-md-6">
 										<label>First Name</label>
 											<div class="input-group">
 												<span class="input-group-addon">
 													<i class="nc-icon nc-single-02"></i>
 												</span>
-												<input name="first_name" type="text" class="form-control" placeholder="First Name">
+												<input name="first_name" type="text" class="form-control" placeholder="<?php echo $_SESSION['first_name']; ?>">
 										</div>
 									</div>
 									<div class="col-md-6">
@@ -125,7 +173,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST')
 											<span class="input-group-addon">
 												<i class="nc-icon nc-single-02"></i>
 											</span>
-											<input name="last_name" type="text" class="form-control" placeholder="Last Name">
+											<input name="last_name" type="text" class="form-control" placeholder="<?php echo $_SESSION['last_name'];?>">
 										</div>
 									</div>
 								</div><!--ends first row-->
@@ -135,30 +183,25 @@ if($_SERVER['REQUEST_METHOD'] == 'POST')
 									<span class="input-group-addon">
 										<i class="nc-icon nc-tag-content"></i>
 									</span>
-									<input name="title" value="<?php echo $_SESSION['title']; ?>" type="text" class="form-control" placeholder="Title">
+									<input name="title" value="<?php echo $_SESSION['title']; ?>" type="text" class="form-control" placeholder="<?php echo $_SESSION['title']; ?>">
 								</div>
 
 								<label>Description</label>
-								<textarea name="description" class="form-control" rows="4" placeholder="Tell everyone a little about you..."></textarea>
+								<textarea name="description" class="form-control" rows="4" placeholder="<?php echo $_SESSION['descr']; ?>"></textarea>
+                <!-- ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                //////////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
+
+
+                <label>Upload A New Profile Picture:</label>
+                <input type="file" name="upload">
+                <label><?php if($ret){echo $ret;}  ?></label>
+
+
+                <!-- ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                //////////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
 								<div class="row">
 									<div class="col-md-4 ml-auto mr-auto text-center">
 										<button class="btn btn-danger btn-lg btn-fill" type="submit">Update</button>
-                    <!-- ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-                    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
-
-
-
-
-
-                    <btn class="btn btn-outline-default btn-round" type="submit" value="Submit"><i class="fa fa-cog"></i>Settings</btn>
-                     <br />
-                    <input type="file" name="upload">
-                    <br />
-                    <input type="submit">
-
-
-                    <!-- ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-                    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
 									</div>
 								</div>
 								</form>
